12c99758aeeee47921bd2bcc7011e1c8fcbe6721
aleksaelezovic <aleksaelezovic@gmail.com>
2025-10-13 13:25:50 +0200

    get block type

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,47 @@
+import re
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    ULIST = "unordered_list"
+    OLIST = "ordered_list"
+
+
+def block_to_block_type(block):
+    if re.match(r"^#{1,6} ", block) is not None:
+        return BlockType.HEADING
+    if block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    is_quote = True
+    for line in block.split("\n"):
+        if not line.startswith(">"):
+            is_quote = False
+            break
+    if is_quote:
+        return BlockType.QUOTE
+    is_ulist = True
+    for line in block.split("\n"):
+        if not line.startswith("- "):
+            is_ulist = False
+            break
+    if is_ulist:
+        return BlockType.ULIST
+    is_olist = True
+    i = 0
+    for line in block.split("\n"):
+        i = i + 1
+        if not line.startswith(f"{i}. "):
+            is_olist = False
+            break
+    if is_olist:
+        return BlockType.OLIST
+    return BlockType.PARAGRAPH
+
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     filtered_blocks = []
