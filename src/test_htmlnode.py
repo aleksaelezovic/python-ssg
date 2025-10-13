1b4e09fbcd38ae2323992c60b69e52ab84372e12
aleksaelezovic <aleksaelezovic@gmail.com>
2025-10-13 12:09:59 +0200

    add leafnode and tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -27,6 +27,22 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode(tag, value, None, props)
         expected = ' class="color-yellow" id="p12"'
         self.assertEqual(expected, node.props_to_html())
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual("<p>Hello, world!</p>", node.to_html())
+
+    def test_leaf_to_html_with_props(self):
+        props = {"CLass": "color-blue", "ID": "p4"}
+        node = LeafNode("p", "Hello, world!", props)
+        expected = '<p class="color-blue" id="p4">Hello, world!</p>'
+        self.assertEqual(expected, node.to_html())
+
+    def test_leaf_to_html_raw(self):
+        node = LeafNode(None, "a text Node")
+        self.assertEqual("a text Node", node.to_html())
 
 
 if __name__ == "__main__":
