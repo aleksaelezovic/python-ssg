e4a9ce9c1b7fcba5c12c15d3b96aab0c876cb87c
aleksaelezovic <aleksaelezovic@gmail.com>
2025-10-13 16:00:57 +0200

    copy assets to public dir

--- src/main.py
+++ src/main.py
@@ -1,13 +1,31 @@
-from textnode import TextNode, TextType
+import os
+import shutil
 
 
 def main():
-    t = TextNode(
-        "This is some anchor text",
-        TextType.LINK,
-        "https://www.boot.dev",
-    )
-    print(t)
+    cp_files("./static", "./public")
+
+
+def extract_title(markdown):
+    for line in markdown.split("\n"):
+        if line.startswith("# "):
+            return line[2:]
+    raise Exception("no title in markdown")
+
+
+def cp_files(src, dest):
+    src = os.path.abspath(src)
+    dest = os.path.abspath(dest)
+    if not os.path.exists(src):
+        raise Exception("source directory does not exist")
+    shutil.rmtree(dest, ignore_errors=True)
+    os.makedirs(dest, exist_ok=True)
+
+    for d in os.listdir(src):
+        if os.path.isfile(os.path.join(src, d)):
+            shutil.copy(os.path.join(src, d), dest)
+        else:
+            cp_files(os.path.join(src, d), os.path.join(dest, d))
 
 
 if __name__ == "__main__":
